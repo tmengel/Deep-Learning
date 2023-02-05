8db81ab81ab4f83f36e1b40603bad7127da6fdea
tmengel <tmengel@vols.utk.edu>
2023-02-05 03:30:16 -0500

    Developed a couple of classes for activation and loss functions

---
+++ project1/ANN.py
@@ -0,0 +1,268 @@
+############################################
+# Authors: Tanner Mengel and Ian Cox
+# Date: 2/4/2022
+# Project 1 - Artificial Neural Networks
+
+############################################
+
+import numpy as np
+import sys
+
+
+
+############################################
+
+class Activation:
+    ''' 
+    Class for activation functions and their derivatives also a method to create custom activation functions
+    '''
+    # constructor that checks if f and df are callable
+    def __init__(self, f, df, name=None):
+        # confirm that f and df are callable
+        if not callable(f) or not callable(df):
+            raise ValueError("Activation function and derivative must be callable\nExample : f = lambda x: x**2, df = lambda x: 2*x")
+        self._function = f
+        self._derivative = df
+        self._name = name if name is not None else "custom"
+
+    # Predefined activation functions and their derivatives
+    @staticmethod
+    def linear(): # static method to return a linear activation function
+        return Activation(lambda x: x, lambda x: 1, "linear")
+    
+    @staticmethod 
+    def logistic(): # static method to return a logistic activation function
+        return Activation(lambda x: 1/(1+np.exp(-x)), lambda x: x * (1 - x), "logistic")
+    
+    # Method to return user defined activation function
+    @staticmethod
+    def custom(f, df, name=None): # static method to return a custom activation function
+        return Activation(f, df, name if name is not None else "custom")
+
+    @classmethod
+    def get(cls, name): # class method to return an activation function from a string
+        if name == "linear": return cls.linear()
+        elif name == "logistic": return cls.logistic()
+        else: raise ValueError("Activation function not found")
+        
+    # call properties to return the activation function and its derivative or the name of the activation function
+    def df(self, x): return self._derivative(x)
+    def f(self, x): return self._function(x)
+    def name(self): return self._name
+   
+class Loss:
+    '''
+    Class for loss functions and user defined loss function 
+    '''
+    def __init__(self, f, df, name=None): 
+        if not callable(f) or not callable(df): # confirm that f and df are callable
+            raise ValueError("Loss function and derivative must be callable\nExample : f = lambda y, yhat: np.mean((y - yhat)**2), df = lambda y, yhat: 2*(yhat - y)")
+        self._name = name if name is not None else "custom"
+        self._function = f
+        self._derivative = df
+        
+    # Predefined loss functions are mean squared error and binary cross entropy
+    @staticmethod
+    def mse(): # static method to return a mean squared error loss function
+        return Loss(lambda y, yhat: np.mean((yhat- y )**2), lambda y, yhat: 2*(yhat - y), "mse")
+    @staticmethod
+    def bce(): # static method to return a binary cross entropy loss function
+        return Loss(lambda y, yhat: -np.mean(y*np.log(yhat) + (1-y)*np.log(1-yhat)), lambda y, yhat: (yhat - y)/(yhat*(1-yhat)), "bce")
+    @staticmethod
+    def custom(f, df, name=None): # static method to return a custom loss function
+        return Loss(f, df, name if name is not None else "custom")
+        
+    @classmethod
+    def get(cls, name): # class method to return a loss function from a string
+        if name == "mse" or name == "MSE" or name == "mean_squared_error": return cls.mse()
+        elif name == "bce" or name == "BCE" or name == "binary_cross_entropy": return cls.bce()
+        else : raise ValueError("Loss function not found")
+    
+    # call properties to return the loss function and its derivative or the name of the loss function
+    def loss(self, y, yhat): return self._function(y, yhat)  
+    def derivatderivativeive(self, y, yhat): return self._derivative(y, yhat)
+    def name(self): return self._name  
+    
+    # function to calculate the loss and derivative of the loss without creating a Loss object
+    def MSE(y, yhat): return np.mean((yhat - y)**2)
+    def BCE(y, yhat): return -np.mean(y*np.log(yhat) + (1-y)*np.log(1-yhat))
+      
+############################################
+
+class Neuron:
+    '''
+    This class contains the methods for a single neuron
+    '''
+    # Neuronal properties and setters
+    @property
+    def weights(self): return self._weights
+    @property
+    def inputs(self): return self._inputs
+    @property
+    def outputs(self): return self._outputs
+    @property
+    def nets(self): return self._nets
+    @property
+    def delta(self): return self._delta
+    @property
+    def dW(self): return self._dW
+    
+    @weights.setter
+    def weights(self, x): self._weights = x
+    @inputs.setter
+    def inputs(self, x): self._inputs = x
+    @outputs.setter
+    def outputs(self, x): self._outputs = x
+    @nets.setter
+    def nets(self, x): self._nets = x
+    @delta.setter
+    def delta(self, x): self._delta = x
+    @dW.setter
+    def dW(self, x): self._dW = x
+        
+    def __init__(self, input_dim, activation, learning_rate=0.1, weights=None): 
+        self.learning_rate = learning_rate
+        self.weights = weights if weights is not None else np.random.rand(input_dim) # if weights is not specified, initialize the weights randomly
+        # check that the weights are the correct shape
+        if self.weights.shape != (input_dim,): 
+            raise ValueError("Weights must be a vector of length input_dim")
+        
+        # check if activation a string or an Activation object
+        self.activation = activation if isinstance(activation, Activation) else Activation.get(activation)
+    
+             
+    #This method returns the activation of the net
+    def activate(self,net):
+        print('activate')
+        return self.activation.f(net)
+    
+    #Calculate the output of the neuron should save the input and output for back-propagation.   
+    def calculate(self,input):
+        print('calculate')
+        self.inputs = input
+        self.nets = np.dot(self.weights, self.inputs)
+        self.outputs = self.activation.f(self.nets)
+        return self.output
+        
+    #This method returns the derivative of the activation function with respect to the net   
+    def activationderivative(self):
+        print('activationderivative')  
+        return self.activation.df(self.outputs)
+      
+    #This method calculates the partial derivative for each weight and returns the delta*w to be used in the previous layer
+    def calcpartialderivative(self, wtimesdelta):
+        print('calcpartialderivative') 
+        self.delta = wtimesdelta*self.activationderivative()
+        self.dW = self.inputs*self.delta
+        return self.delta*self.weights
+               
+    #Simply update the weights using the partial derivatives and the leranring weight
+    def updateweight(self):
+        print('updateweight') 
+        self.weights -= self.learning_rate*self.dW
+     
+        
+############################################
+       
+class FullyConnected:
+    '''
+    Class manages neurons in a fully connected layer, initializes the neurons and calculates the output of the layer
+    '''
+    
+    # Layer properties and setters
+    @property
+    def weights(self): return self._weights
+    @property
+    def inputs(self): return self._inputs
+    @property
+    def outputs(self): return self._outputs
+    @property
+    def deltaw(self): return self._deltaw
+
+    @weights.setter
+    def weights(self, x): self._weights = x
+    @inputs.setter
+    def inputs(self, x): self._inputs = x
+    @outputs.setter
+    def outputs(self, x): self._outputs = x
+    @deltaw.setter
+    def deltaw(self, x): self._deltaw = x
+ 
+    
+    def __init__(self, neuron_num, activation, input_num, learning_rate=0.1, weights=None):
+        print('constructor') 
+        self.weights = weights if weights is not None else np.random.rand(neuron_num,input_num) #if weights is not specified, initialize the weights randomly. 
+        self.neurons = [Neuron(input_num, activation, learning_rate, self.weights[i,:]) for i in range(neuron_num)] #create a list of neurons with the correct number of inputs and weights
+                
+    #calcualte the output of all the neurons in the layer and return a vector with those values (go through the neurons and call the calcualte() method)      
+    def calculate(self, input):
+        print('calculate') 
+        self.inputs = input 
+        self.outputs = np.array([neuron.calculate(input) for neuron in self.neurons])        
+        return self.outputs
+    
+    #given the next layer's w*delta, should run through the neurons calling calcpartialderivative() for each (with the correct value), sum up its ownw*delta, and then update the wieghts (using the updateweight() method). I should return the sum of w*delta.          
+    def calcwdeltas(self, wtimesdelta):
+        print('calcwdeltas') 
+        # go through the neurons and call calcpartialderivative() for each
+        self.deltaw  = np.zeros(self.input.shape)
+        for neuron in self.neurons:
+            self.deltaw += neuron.calcpartialderivative(wtimesdelta)
+            neuron.updateweight()
+        # update the weights
+        return self.deltaw
+
+############################################
+    
+class NeuralNetwork:
+
+   def __init__(self, num_of_layers, num_of_neurons, input_size, activation, loss, learning_rate=0.1, weights=None):
+       
+        # check that the number of neurons is the correct length
+        if len(num_of_neurons) != num_of_layers:
+            raise ValueError("num_of_neurons must be a vector of length num_of_layers")
+
+        self.architecture = [input_size , *num_of_neurons , 1] # add the input size and output size to the architecture
+        # check that the weights are the correct shape
+        for i in range(len(self.architecture)-1):
+            if self.weights[i].shape != (self.architecture[i],self.architecture[i+1]):
+                raise ValueError("Weights must be a vector of length num_of_layers")
+            
+        self.weights = weights if weights is not None else np.array([np.random.rand(self.architecture[i],self.architecture[i+1]) for i in range(len(self.architecture)-1)])
+        self.Network = [FullyConnected(self.architecture[i+1],self.architecture[i],activation,learning_rate,self.weights[i]) for i in range(len(self.architecture)-1)]
+        self.loss = Loss.get(loss)
+        
+        
+        
+        #Given an input, calculate the output (using the layers calculate() method)
+        def calculate(self,input):
+            print('constructor')
+            
+        #Given a predicted output and ground truth output simply return the loss (depending on the loss function)
+        def calculateloss(self,yp,y):
+            print('calculate')
+        
+        #Given a predicted output and ground truth output simply return the derivative of the loss (depending on the loss function)        
+        def lossderiv(self,yp,y):
+            print('lossderiv')
+        
+        #Given a single input and desired output preform one step of backpropagation (including a forward pass, getting the derivative of the loss, and then calling calcwdeltas for layers with the right values         
+        def train(self,x,y):
+            print('train')
+            
+            
+if __name__=="__main__":
+    if (len(sys.argv)<2):
+        print('a good place to test different parts of your code')
+        
+    elif (sys.argv[1]=='example'):
+        print('run example from class (single step)')
+        w=np.array([[[.15,.2,.35],[.25,.3,.35]],[[.4,.45,.6],[.5,.55,.6]]])
+        x==np.array([0.05,0.1])
+        np.array([0.01,0.99])
+        
+    elif(sys.argv[1]=='and'):
+        print('learn and')
+        
+    elif(sys.argv[1]=='xor'):
+        print('learn xor')
